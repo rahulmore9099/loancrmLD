d68317c51e535c2415e1ebbd36cd9652c2c0fdce
rahulmore007 <49087648+rahulmore007@users.noreply.github.com>
2026-02-25 12:07:27 +0530

    Add runtime file

--- website/views.py
+++ website/views.py
@@ -45,26 +45,55 @@ def login_view(request):
 
 # ================= SIGNUP =================
 
+
+
 def signup_view(request):
 
-    if request.method == "POST":
+    if request.method == 'POST':
 
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        # username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
 
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Username already exists")
-            return redirect("signup")
+        if password1 != password2:
+            messages.error(request, "Passwords do not match")
+            return redirect('signup')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+            return redirect('signup')
 
         User.objects.create_user(
-            username=username,
-            password=password
+            username=email.split('@')[0],  # Use email username
+            email=email,
+            password=password1
         )
 
-        messages.success(request, "Account Created Successfully")
-        return redirect("login")
+        messages.success(request, "Account created successfully")
+        return redirect('login')
 
-    return render(request, "website/signup.html")
+    return render(request, 'website/signup.html')
+# def signup_view(request):
+
+#     if request.method == "POST":
+
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
+
+#         if User.objects.filter(username=username).exists():
+#             messages.error(request, "Username already exists")
+#             return redirect("signup")
+
+#         User.objects.create_user(
+#             username=username,
+#             password=password
+#         )
+
+#         messages.success(request, "Account Created Successfully")
+#         return redirect("login")
+
+#     return render(request, "website/signup.html")
 
 
 # ================= LOGOUT =================
